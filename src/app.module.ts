97729fe1ac05a406a643a28bd104eb7d8fb0bee2
edsonstudio <edsont8@hotmail.com>
2024-01-03 09:26:59 -0300

    Registrado o gateway nos providers do app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ChatGateway } from './chat.gateway';
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [],
+  providers: [ChatGateway],
 })
 export class AppModule {}
