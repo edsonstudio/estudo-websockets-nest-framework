039abc41d25a94efef8072aa81e54efe1fd1aac3
edsonstudio <edsont8@hotmail.com>
2024-01-03 11:38:23 -0300

    Corrigidos BUGs no CORS, ajustado template do client-side, e corrigidas as logicas que exibem os usuarios conectados ao chat

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,36 +1,67 @@
 import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 
+const options = {
+    cors: {
+        origin: ["http://localhost:8080", "http://127.0.0.1:8080"],
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        credentials: true
+    }
+}
+
 // @WebSocketGateway(5000) // É possível selecionar a porta em que o gateway será executado
-@WebSocketGateway()
+@WebSocketGateway(options)
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
-    @WebSocketServer()
+
+    @WebSocketServer() server;
+
+    private clients: { [key: string]: any } = {};
 
     handleConnection(client: any) {
         console.log(`${client.id} conectado!`);
-        client.broadcast.emit('users', {
-            user: client.id,
+        this.clients[client.id] = client;
+
+        const connectedUsers = Object.keys(this.clients);
+
+        // Emitir para o cliente recém-conectado
+        client.emit('users', {
+            users: connectedUsers,
+            action: 'connected'
+        });
+
+        // Emitir para todos os clientes conectados
+        this.server.emit('users', {
+            users: connectedUsers,
             action: 'connected'
         });
     }
 
     handleDisconnect(client: any) {
         console.log(`${client.id} desconectado!`);
-        client.broadcast.emit('users', {
+        delete this.clients[client.id];
+
+        this.server.emit('users', {
             user: client.id,
             action: 'disconnected'
         });
     }
 
     @SubscribeMessage('chat')
-    chat(client: any, data: any) { 
+    chat(client: any, data: any) {
         console.log(data);
+
+        // Modificando a estrutura da mensagem para incluir o ID do remetente
+        const messageWithUser = {
+            user: client.id,
+            message: data.message
+        };
+
         // client.emit(client.id, 'Mensagem'); // Para enviar mensagem para um client específico
-        client.broadcast.emit('chat', data);
-        return data;
+        client.broadcast.emit('chat', messageWithUser);
+        return messageWithUser;
     }
 
     @SubscribeMessage('users')
-    users(client: any, data: any) { 
+    users(client: any, data: any) {
         console.log(data);
         return data;
     }
